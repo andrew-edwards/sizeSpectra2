ccdfcaa401eb0b50a0f757419056161ee4385e13
andrew-edwards <Andrew.Edwards@dfo-mpo.gc.ca>
2026-05-22 10:11:25 -0700

    More TODOs, add an error check and test.

--- R/determine-xmin-based-on-hist.R
+++ R/determine-xmin-based-on-hist.R
@@ -10,11 +10,17 @@
 ##' @author Andrew Edwards
 ##' @examples
 ##' \dontrun{
-##'  TODO
+##' sim_vec_binned_2 <- sim_vec_binned
+##' sim_vec_binned_2[1, "bin_count"] <- 100  # lower count for first bin
+##' hh <- make_hist_for_binned_counts_mlebin(sim_vec_binned)
+##' x_min <- determine_xmin_based_on_hist(hh)
+##' x_min
+##'
+##' # The above gets calculated automatically within:
+##' res_binned_2 <- determine_xmin_and_fit_mlebin(sim_vec_binned_2)
+##' plot(res_binned_2)
 ##' }
 determine_xmin_based_on_hist <- function(h){
-  # changed function name to determine_xmin_based_on_hist, since adapting in the
-  # various calls to have xmin not be the bin break.  TODO remove that?
   stopifnot("h needs to be a histogram object" =
               class(h) == "histogram")
 
@@ -22,11 +28,11 @@ determine_xmin_based_on_hist <- function(h){
                     which.max(h$counts),  # returns the first one if they are ties
                     which.max(h$density)) # for unequal bin widths
 
-  # TODO I'd commented this out, I guess because it might still fit, but the
-  # resulting plots should clearly show the issue. Make a test to see if still fits.
-  # if(max_ind == length(h$counts)){
-  #   stop("The maximum count is in the final bin, so a descending PLB is not appropriate.")
-  # }     # Also the fitting code will likely break.
+  # Give error (since fitting code breaks anyway) if end up with only one bin;
+  #  in the testing I check that it still works with only two bins
+  if(max_ind == length(h$counts)){
+     stop("The maximum count is in the final bin, so a descending PLB is not appropriate and will not be fit.")
+  }
 
   x_min <- h$breaks[max_ind]
 
--- tests/testthat/test-mlebin.R
+++ tests/testthat/test-mlebin.R
@@ -70,4 +70,23 @@ test_that("MLEbin fitting and plotting works and matches original results", {
 
   expect_equal(make_hist(1:10)$breaks[10],
                10)
+
+  # Check what happens when only one bin gets fit
+  sim_vec_binned_3 <- sim_vec_binned
+  sim_vec_binned_3[9, "bin_count"] <- 1000000  # big count for last bin (gets normalised)
+
+  expect_error(determine_xmin_and_fit_mlebin(sim_vec_binned_3))
+
+  # And it should still run if end up with only two bins
+  sim_vec_binned_4 <- sim_vec_binned
+  sim_vec_binned_4[8:9, "bin_count"] <- c(100000,
+                                          100000) # big count for penultimate
+                                          # and final bin (gets normalised)
+
+  res_binned_4 <- determine_xmin_and_fit_mlebin(sim_vec_binned_4)
+  expect_equal(res_binned_4$mlebin_fit$b_mle,
+               -0.999995327)             # -1, since
+
+  # And plotting still works, even for only two bins
+  expect_invisible(plot(res_binned_4))
 })
